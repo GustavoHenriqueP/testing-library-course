31357d3a622bfb9c3626e41feaa52330cfed14f2
Gustavo Henrique <gustavo_henriquedepaula@hotmail.com>
2024-07-17 21:36:20 -0300

    finalizar vite-starter project

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import './App.css';
+import { kebabCaseToTitleCase } from './helpers';
 
 function App() {
-  const [buttonColor, setButtonColor] = React.useState('red');
-  const nextColor = buttonColor === 'red' ? 'blue' : 'red';
+  const [disabled, setDisabled] = React.useState(false);
+  const [buttonColor, setButtonColor] = React.useState('medium-violet-red');
+  const nextColorClass =
+    buttonColor === 'medium-violet-red' ? 'midnight-blue' : 'medium-violet-red';
+  const nextColorTitle = kebabCaseToTitleCase(nextColorClass);
 
   return (
     <div>
-      <button className={buttonColor} onClick={() => setButtonColor(nextColor)}>
-        Change to {nextColor}
+      <button
+        className={disabled ? 'gray' : buttonColor}
+        onClick={() => setButtonColor(nextColorClass)}
+        disabled={disabled}
+      >
+        Change to {nextColorTitle}
       </button>
       <br />
-      <input type="checkbox" id="disabled-button-checkbox" defaultChecked={false} />
+      <input
+        type="checkbox"
+        id="disabled-button-checkbox"
+        defaultChecked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
       <label htmlFor="disabled-button-checkbox">Disable button</label>
     </div>
   );
--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,8 +1,12 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { kebabCaseToTitleCase } from './helpers';
 
 //* A instrutora comenta que no backend é realmente interessante deixar cada teste o mais unitário e separado possível.
 //* Porém, no frontend isso não é exatamente necessário, já que normalmente queremos testar um "fluxo";
+
+//* Veremos bastante código repetido. Mas quando estamos testando código, é menos importante aplicar o DRY
+//* do que ser capoaz de depurar rapidamente o código que falha
 test('button click flow', () => {
   //* logRoles é útil quanda temos um componente mais complexo e não sabemos todas as roles que temos
   // const { container } = render(<App />);
@@ -15,7 +19,7 @@ test('button click flow', () => {
   const buttonElement = screen.getByRole('button', { name: /blue/i }); // regex with case insensitive (i) - RTL
 
   // check initial color
-  expect(buttonElement).toHaveClass('red'); // Expect: Vitest - .toHaveClass(): Matcher from jest-DOM
+  expect(buttonElement).toHaveClass('medium-violet-red'); // Expect: Vitest - .toHaveClass(): Matcher from jest-DOM
 
   // click the button
   fireEvent.click(buttonElement);
@@ -24,7 +28,7 @@ test('button click flow', () => {
   expect(buttonElement).toHaveTextContent(/red/i);
 
   // check the button color
-  expect(buttonElement).toHaveClass('blue');
+  expect(buttonElement).toHaveClass('midnight-blue');
 });
 
 test('checkbox flow', () => {
@@ -37,4 +41,49 @@ test('checkbox flow', () => {
   // check initial conditions
   expect(buttonElement).toBeEnabled();
   expect(checkboxElement).not.toBeChecked();
+
+  // click checkbox to disable button
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeDisabled();
+  expect(buttonElement).toHaveClass('gray');
+
+  // click on checkbox again - re-enable button
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeEnabled();
+  expect(buttonElement).toHaveClass('medium-violet-red');
+});
+
+test('checkbox flow after button click', () => {
+  // render the app
+  render(<App />);
+
+  // find elements
+  const buttonElement = screen.getByRole('button', { name: /blue/i });
+  const checkboxElement = screen.getByRole('checkbox', { name: /disable button/i });
+
+  // change button color
+  fireEvent.click(buttonElement);
+
+  // disable blue button
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeDisabled();
+  expect(buttonElement).toHaveClass('gray');
+
+  // re-enable blue button
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeEnabled();
+  expect(buttonElement).toHaveClass('midnight-blue');
+});
+
+//* Unit testing
+describe('kebabCaseToTitleCase', () => {
+  test('works for no hyphens', () => {
+    expect(kebabCaseToTitleCase('red')).toBe('Red');
+  });
+  test('works for one hyphen', () => {
+    expect(kebabCaseToTitleCase('midnight-blue')).toBe('Midnight Blue');
+  });
+  test('works for multiple hyphens', () => {
+    expect(kebabCaseToTitleCase('medium-violet-red')).toBe('Medium Violet Red');
+  });
 });
